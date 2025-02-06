6f99fa02fdf3bb1ed79ed79de7e4b443d18149e9
19kummitha <113335352+19kummitha@users.noreply.github.com>
2025-02-06 16:17:42 +0530

    added gitignore
    made the asynchronous methods in product controller and added gitignore file to make git repo clean and looks good

--- API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f45aa5c66ca9998047f38bd4f0d7fbf5ffd92c8c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3d760fa8126189332582444707e3ffd631bfa1f7")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
