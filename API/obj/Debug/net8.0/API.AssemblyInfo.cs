a274da8706c42cf8781005fbe17fcb635be2fe35
19kummitha <113335352+19kummitha@users.noreply.github.com>
2025-02-07 10:39:10 +0530

    React client
    Added React app for client application.

--- API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3d760fa8126189332582444707e3ffd631bfa1f7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6f99fa02fdf3bb1ed79ed79de7e4b443d18149e9")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ client/src/App.tsx
@@ -0,0 +1,5 @@
+function App() {
+  return <div style={{ fontSize: "1.2rem" }}>Re-Store</div>;
+}
+
+export default App;
